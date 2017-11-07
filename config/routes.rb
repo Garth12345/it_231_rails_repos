1a7109c6f89547f1c342a0c12c1b1b974a4b4a73
Garth12345 <garthadcock1@gmail.com>
2017-11-07 20:23:24 +0000

    IT-231 Lab#9 Update

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  resources :orders
+  resources :line_items
+  resources :carts
+  get 'store/index'
+
+  resources :products
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   root 'products#index'
+   #root 'store#index'
+end
