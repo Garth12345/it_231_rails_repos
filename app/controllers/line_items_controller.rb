91f442241caaf41f9703c3602822f47bc8c2fbbb
Garth12345 <garthadcock1@gmail.com>
2017-11-09 18:00:48 +0000

    lab 9 update

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -39,7 +39,7 @@ class LineItemsController < ApplicationController
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
-  end
+ end
 
   # PATCH/PUT /line_items/1
   # PATCH/PUT /line_items/1.json
